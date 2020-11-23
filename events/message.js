06edc442a103ae867f4d084e0ef53f5db1722632
Thomas Bnt <contact@thomasbnt.fr>
2020-11-23 18:39:19 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js')
 const embed = new Discord.RichEmbed()
 module.exports = async (bot, msg) => {
 
+  
+  /*
+  * Don't delete that if you dont wan't to bot respond to a another bot, in private message and himself.  
+  */
   if (msg.author.bot) return
   if (msg.author.id === bot.user.id) return
   if (msg.channel.recipient) return
